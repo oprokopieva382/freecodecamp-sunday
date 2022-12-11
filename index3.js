8dd0da8aa9bf9cd55ca01848c7a17097e71a3658
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 16:25:54 -0600

    findLongestWordLength call

--- index3.js
+++ index3.js
@@ -1,21 +1,24 @@
 //Convert Celsius to Fahrenheit
-function convertCtoF(celsius) {
-  let fahrenheit = celsius * (9 / 5) + 32;
-  return fahrenheit;
-}
-convertCtoF(30);
-//Reverse a String
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
-reverseString("hello");
-//Factorialize a Number
-function factorialize(num) {
-  if (num === 0) {
-    return 1;
-  }
-  return num * factorialize(num - 1);
-}
-factorialize(5);
+// function convertCtoF(celsius) {
+//   let fahrenheit = celsius * (9 / 5) + 32;
+//   return fahrenheit;
+// }
+// convertCtoF(30);
+// //Reverse a String
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+// reverseString("hello");
+// //Factorialize a Number
+// function factorialize(num) {
+//   if (num === 0) {
+//     return 1;
+//   }
+//   return num * factorialize(num - 1);
+// }
+// factorialize(5);
 //Find the Longest Word in a String
-function findLongestWordLength(str) {}
+function findLongestWordLength(str) {
+  return str.splite("");
+}
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
