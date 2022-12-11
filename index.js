9217adfbf3512d971c901bae01be105459770e5b
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 15:23:18 -0600

    Add Key-Value Pairs to JavaScript Objects

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ let myNestedArray = [
   ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
   ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]],
 ];
+//Add Key-Value Pairs to JavaScript Objects
