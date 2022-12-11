fd22a323de22eb1ee9336771fdc09227dd9d354b
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 15:21:30 -0600

    myNestedArray2

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 //Create complex multi-dimensional arrays
-let myNestedArray = [["unshift", false, 1, 2, 3, "complex", "nested"]];
+let myNestedArray = [
+  ["unshift", false, 1, 2, 3, "complex", "nested"],
+  ["loop", "shift", 6, 7, 1000, "method"],
+];
