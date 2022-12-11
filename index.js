ced93c9c569f16beafde4a56e881c3917ed07799
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 15:22:15 -0600

    myNestedArray4

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let myNestedArray = [
   ["unshift", false, 1, 2, 3, "complex", "nested"],
   ["loop", "shift", 6, 7, 1000, "method"],
   ["concat", false, true, "spread", "array", ["deep"]],
+  ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
 ];
