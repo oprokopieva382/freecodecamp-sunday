aefad1dd111fbff49743b5c8064c5a84902ebafd
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 15:38:50 -0600

    users object

--- index2.js
+++ index2.js
@@ -25,4 +25,21 @@ delete foods.plums;
 delete foods.strawberries;
 console.log(foods);
 //Check if an Object has a Property
-let users = {};
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
