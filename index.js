89cd7161eb49edf9813dd80931b952db7b65b079
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 15:27:51 -0600

    userActivity object

--- index.js
+++ index.js
@@ -13,4 +13,11 @@ foods["grapes"] = 35;
 foods["strawberries"] = 27;
 console.log(foods);
 //Modify an Object Nested Within an Object
-let userActivity = {};
+let userActivity = {
+  id: 23894201352,
+  date: "January 1, 2017",
+  data: {
+    totalUsers: 51,
+    online: 42,
+  },
+};
