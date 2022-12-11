e615f33b9d36e8edff02c82cd25574269412f987
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 15:46:54 -0600

    const users

--- index2.js
+++ index2.js
@@ -25,35 +25,36 @@ delete foods.plums;
 delete foods.strawberries;
 console.log(foods);
 //Check if an Object has a Property
-let users = {
-  Alan: {
-    age: 27,
-    online: true,
-  },
-  Jeff: {
-    age: 32,
-    online: true,
-  },
-  Sarah: {
-    age: 48,
-    online: true,
-  },
-  Ryan: {
-    age: 19,
-    online: true,
-  },
-};
+// let users = {
+//   Alan: {
+//     age: 27,
+//     online: true,
+//   },
+//   Jeff: {
+//     age: 32,
+//     online: true,
+//   },
+//   Sarah: {
+//     age: 48,
+//     online: true,
+//   },
+//   Ryan: {
+//     age: 19,
+//     online: true,
+//   },
+// };
 
-function isEveryoneHere(userObj) {
-  if (
-    userObj.hasOwnProperty("Alan") &&
-    userObj.hasOwnProperty("Jeff") &&
-    userObj.hasOwnProperty("Sarah") &&
-    userObj.hasOwnProperty("Ryan")
-  ) {
-    return true;
-  }
-  return false;
-}
-console.log(isEveryoneHere(users));
+// function isEveryoneHere(userObj) {
+//   if (
+//     userObj.hasOwnProperty("Alan") &&
+//     userObj.hasOwnProperty("Jeff") &&
+//     userObj.hasOwnProperty("Sarah") &&
+//     userObj.hasOwnProperty("Ryan")
+//   ) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isEveryoneHere(users));
 //Iterate Through the Keys of an Object with a for...in Statement
+const users = {};
