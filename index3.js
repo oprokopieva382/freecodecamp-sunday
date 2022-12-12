b348896bca5946ecd54760621383f4b23c9aaf42
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:36:47 -0600

    return arr

--- index3.js
+++ index3.js
@@ -83,4 +83,6 @@ function frankenSplice(arr1, arr2, n) {
   return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
 }
 //Falsy Bouncer
-function bouncer(arr) {}
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
