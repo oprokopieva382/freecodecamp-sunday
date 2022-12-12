2ed2ecbb848e7cf4aa0a3a6dac32255a2e694384
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 20:35:08 -0600

    Return Largest Numbers in Arrays

--- index3.js
+++ index3.js
@@ -22,3 +22,4 @@ function findLongestWordLength(str) {
   return Math.max(...str.split(" ").map((word) => word.length));
 }
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+//Return Largest Numbers in Arrays
