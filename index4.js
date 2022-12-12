f3dd4ca170636e91208747a6e91221e486fe69d6
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 22:11:45 -0600

    function House

--- index4.js
+++ index4.js
@@ -33,3 +33,8 @@ function Dog(name, color) {
 }
 let terrier = new Dog("Bob", "gray");
 //Verify an Object's Constructor with instanceof
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+let myHouse = new House(5);
+myHouse instanceof House;
