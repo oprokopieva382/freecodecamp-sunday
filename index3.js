f52cc30ec628e66009a5a245f4183aa02054d4b9
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:48:31 -0600

    for loop added

--- index3.js
+++ index3.js
@@ -97,4 +97,11 @@ function getIndexToIns(arr, num) {
 }
 getIndexToIns([40, 60], 50);
 //Mutations
-function mutation(arr) {}
+function mutation(arr) {
+  const test = arr[1].toLowerCase();
+  const target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
