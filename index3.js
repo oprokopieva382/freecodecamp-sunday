7269763aa5754c4e4e47dc07b5661c25b1ac3cd5
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 20:35:57 -0600

    largestOfFour array

--- index3.js
+++ index3.js
@@ -24,3 +24,9 @@ function findLongestWordLength(str) {
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
 //Return Largest Numbers in Arrays
 function largestOfFour(arr) {}
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
