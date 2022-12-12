6c8c86a7eec203d8cb56327830d7a15f601f8d1e
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:58:51 -0600

    new object dog

--- index4.js
+++ index4.js
@@ -1,8 +1,15 @@
 //Create a Basic JavaScript Object
+// let dog = {
+//   name: "Aflac",
+//   numLegs: 2,
+// };
+// //Use Dot Notation to Access the Properties of an Object
+// console.log(dog.name);
+// console.log(dog.numLegs);
+//Create a Method on an Object
 let dog = {
-  name: "Aflac",
-  numLegs: 2,
+  name: "Spot",
+  numLegs: 4,
 };
-//Use Dot Notation to Access the Properties of an Object
-console.log(dog.name);
-console.log(dog.numLegs);
+
+dog.sayLegs();
