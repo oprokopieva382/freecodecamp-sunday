9a3f17fec48d5070a1e88715ca1632fe7699c727
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:07:50 -0600

    Finders Keepers

--- index3.js
+++ index3.js
@@ -55,3 +55,4 @@ function truncateString(str, num) {
   return str.length > num ? str.slice(0, num) + "..." : str;
 }
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+//Finders Keepers
