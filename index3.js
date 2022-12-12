1a237c98106fc136f3105799bfebdcd9ff1c9230
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:06:40 -0600

    truncateString call

--- index3.js
+++ index3.js
@@ -54,3 +54,4 @@ repeatStringNumTimes("abc", 3);
 function truncateString(str, num) {
   return str.length > num ? str.slice(0, num) + "..." : str;
 }
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
