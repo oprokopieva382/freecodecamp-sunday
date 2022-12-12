8342d888a7186c7a230da3125ee839edabe73dd0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 22:18:04 -0600

    Prototype Properties

--- index4.js
+++ index4.js
@@ -51,3 +51,8 @@ for (let property in canary) {
   }
 }
 //Use Prototype Properties to Reduce Duplicate Code
+function Dog(name) {
+  this.name = name;
+}
+Dog.prototype.numLegs = 4;
+let beagle = new Dog("Snoopy");
