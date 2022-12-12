ef976a1ac43823c8213e54d64aac996f31cace7c
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:12:11 -0600

    findElement call

--- index3.js
+++ index3.js
@@ -59,3 +59,4 @@ truncateString("A-tisket a-tasket A green and yellow basket", 8);
 function findElement(arr, func) {
   return arr.find(func);
 }
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
