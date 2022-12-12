ae0b6e445823c4f84c2c8bfe691a1701499d77e2
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 22:09:33 -0600

    Constructor with Arguments

--- index4.js
+++ index4.js
@@ -26,3 +26,9 @@ dog.sayLegs();
 // }
 // let hound = new Dog();
 //Extend Constructors to Receive Arguments
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+let terrier = new Dog("Bob", "gray");
