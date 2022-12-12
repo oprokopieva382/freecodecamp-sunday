a7619782e2bc8ba761175bccd2e85854cde6f487
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-11 21:18:15 -0600

    return typeof bool

--- index3.js
+++ index3.js
@@ -65,4 +65,6 @@ findElement([1, 2, 3, 4], (num) => num % 2 === 0);
 //   return arr[arr.map(func).indexOf(true)];
 // }
 //Boo who
-function booWho(bool) {}
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
